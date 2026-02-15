7b67f7582c412f6660e1627a44624191002aa088
Priyanshu Kumar Jha <69211610+jhapriyansh@users.noreply.github.com>
2026-02-15 17:44:56 +0530

    Add redirect configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://www.0xpkj.tech/:path*",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
